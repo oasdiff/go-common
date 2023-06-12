08a46e0b46958faf3bec98bf5236ef241170fdcd
effoeffi <effoeffi@gmail.com>
2023-06-12 16:45:24 +0300

    [gcs] mock read data

--- gcs/client_inmemory.go
+++ gcs/client_inmemory.go
@@ -1,11 +1,17 @@
 package gcs
 
-type InMemoryStore struct{}
+type InMemoryStore struct{ pathToFile map[string][]byte }
 
-func NewInMemoryStore() Client { return &InMemoryStore{} }
+func NewInMemoryStore(pathToFile map[string][]byte) Client {
+
+	return &InMemoryStore{pathToFile: pathToFile}
+}
 
 func (m *InMemoryStore) UploadSpec(string, string, []byte) error { return nil }
 
-func (m *InMemoryStore) Read(string) ([]byte, error) { return []byte{}, nil }
+func (m *InMemoryStore) Read(tenant string) ([]byte, error) {
+
+	return m.pathToFile[tenant], nil
+}
 
 func (m *InMemoryStore) Close() error { return nil }
