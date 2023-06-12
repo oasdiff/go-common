ea0cd1d8119201806e6e82d4f5ae085c519252b1
effoeffi <effoeffi@gmail.com>
2023-06-12 13:57:35 +0300

    [storage] read file

--- gcs/client.go
+++ gcs/client.go
@@ -11,13 +11,12 @@ import (
 	"github.com/oasdiff/go-common/env"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
 type Client interface {
 	UploadSpec(tenantId string, name string, file []byte) error
-	ReadLatestSpec(tenantId string) ([]byte, error)
+	Read(path string) ([]byte, error)
 	Close() error
 }
 
@@ -76,23 +75,15 @@ func (store *Store) UploadSpec(tenantId string, name string, file []byte) error 
 	return nil
 }
 
-func (store *Store) ReadLatestSpec(tenantId string) ([]byte, error) {
-
-	path, err := store.getLatestSpec(tenantId)
-	if err != nil {
-		return nil, err
-	}
-	if path == "" {
-		logrus.Infof("no specs for tenant '%s'", tenantId)
-		return []byte{}, nil
-	}
+// path should look like this: {tenant-id}/spec/1685962955
+func (store *Store) Read(path string) ([]byte, error) {
 
 	rc, err := store.client.Bucket(store.bucket).
 		Object(path).
 		NewReader(context.Background())
 	if err != nil {
-		msg := fmt.Sprintf("failed to create reader for file '%s' with '%v' tenant '%s'",
-			path, err, tenantId)
+		msg := fmt.Sprintf("failed to create reader for file '%s' with '%v'",
+			path, err)
 		logrus.Error(msg)
 		return nil, errors.New(msg)
 	}
@@ -104,7 +95,7 @@ func (store *Store) ReadLatestSpec(tenantId string) ([]byte, error) {
 
 	data, err := io.ReadAll(rc)
 	if err != nil {
-		msg := fmt.Sprintf("failed to read file '%s' with '%v' tenant '%s'", path, err, tenantId)
+		msg := fmt.Sprintf("failed to read file '%s' with '%v'", path, err)
 		logrus.Errorf(msg)
 		return nil, errors.New(msg)
 	}
@@ -115,31 +106,4 @@ func (store *Store) ReadLatestSpec(tenantId string) ([]byte, error) {
 func (store *Store) Close() error {
 
 	return store.client.Close()
-}
-
-func (store *Store) getLatestSpec(tenantId string) (string, error) {
-
-	res := ""
-	latestCreated := time.Date(1980, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
-	folder := fmt.Sprintf("%s/spec", tenantId)
-	it := store.client.Bucket(store.bucket).
-		Objects(context.Background(), &storage.Query{Prefix: folder})
-	for {
-		attrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			logrus.Errorf("failed to iterate over '%s' with '%v'", folder, err)
-			return "", err
-		}
-
-		if latestCreated.Before(attrs.Created) {
-			latestCreated = attrs.Created
-			// attrs.Name looks like this: "{tenant-id}/spec/1685962955"
-			res = attrs.Name
-		}
-	}
-
-	return res, nil
 }
--- gcs/client_inmemory.go
+++ gcs/client_inmemory.go
@@ -6,6 +6,6 @@ func NewInMemoryStore() Client { return &InMemoryStore{} }
 
 func (m *InMemoryStore) UploadSpec(string, string, []byte) error { return nil }
 
-func (m *InMemoryStore) ReadLatestSpec(tenantId string) ([]byte, error) { return []byte{}, nil }
+func (m *InMemoryStore) Read(string) ([]byte, error) { return []byte{}, nil }
 
 func (m *InMemoryStore) Close() error { return nil }
