929c26ffb6ff44d5b98ac7ac19bdeb0defdb94d0
effoeffi <effoeffi@gmail.com>
2023-08-29 14:55:25 +0300

    GetTelemetryTableMetadata

--- bq/model.go
+++ bq/model.go
@@ -5,24 +5,29 @@ import (
 	"cloud.google.com/go/civil"
 )
 
+type KeyValue struct {
+	Key   string `bigquery:"key"`
+	Value string `bigquery:"value"`
+}
+
 type Telemetry struct {
-	Id                 string         `bigquery:"id"`
+	Application        string         `bigquery:"application"`
+	ApplicationVersion string         `bigquery:"application_version"`
 	Time               civil.DateTime `bigquery:"time"`
 	MachineId          string         `bigquery:"machine_id"`
 	Runtime            string         `bigquery:"runtime"`
 	Platform           string         `bigquery:"platform"`
 	Command            string         `bigquery:"command"`
 	Args               []string       `bigquery:"args"`
-	Flags              string         `bigquery:"flags"`
-	Application        string         `bigquery:"application"`
-	ApplicationVersion string         `bigquery:"application_version"`
+	Flags              []KeyValue     `bigquery:"flags"`
 }
 
 // Save implements the ValueSaver interface
 func (t *Telemetry) Save() (map[string]bigquery.Value, string, error) {
 
 	return map[string]bigquery.Value{
-		"id":                  t.Id,
+		"application":         t.Application,
+		"application_version": t.ApplicationVersion,
 		"time":                t.Time,
 		"machine_id":          t.MachineId,
 		"runtime":             t.Runtime,
@@ -30,7 +35,28 @@ func (t *Telemetry) Save() (map[string]bigquery.Value, string, error) {
 		"command":             t.Command,
 		"args":                t.Args,
 		"flags":               t.Flags,
-		"application":         t.Application,
-		"application_version": t.ApplicationVersion,
 	}, "", nil
+}
+
+func GetTelemetryTableMetadata() *bigquery.TableMetadata {
+
+	const fieldTime = "time"
+
+	return &bigquery.TableMetadata{Schema: bigquery.Schema{
+		{Name: fieldTime, Type: bigquery.DateTimeFieldType},
+		{Name: "application", Type: bigquery.StringFieldType},
+		{Name: "application_version", Type: bigquery.StringFieldType},
+		{Name: "machine_id", Type: bigquery.StringFieldType},
+		{Name: "runtime", Type: bigquery.StringFieldType},
+		{Name: "platform", Type: bigquery.StringFieldType},
+		{Name: "command", Type: bigquery.StringFieldType},
+		{Name: "args", Type: bigquery.StringFieldType, Repeated: true},
+		{Name: "flags", Type: bigquery.RecordFieldType, Repeated: true, Schema: bigquery.Schema{
+			{Name: "key", Type: bigquery.StringFieldType},
+			{Name: "value", Type: bigquery.StringFieldType},
+		}},
+	}, TimePartitioning: &bigquery.TimePartitioning{
+		Type:  bigquery.DayPartitioningType,
+		Field: fieldTime,
+	}}
 }
